be174a3d692bc390ce703b280b227d42129982b8
Matias <matiasenriquegarciadev@gmail.com>
2021-07-28 11:47:06 -0300

    Login Screen View and some functionality
    json server .
    The login sreen view is almost done.

---
+++ index.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form')
+const user = document.querySelector('useremail')
+const password = document.querySelector('password-input')
+const error = document.querySelector('error')
+
+form.addEventListener('submit', (e) => {
+  let messages =[]
+  if( user === "admin" && password === "admin"){
+    alert("Entered correct");
+    e.preventDefault()
+  }
+  else{
+    document.getElementById('error').style.display='flex';
+    document.getElementById('error-pass').style.display='flex';
+    alert("Entered fail");
+    e.preventDefault()
+  }
+})
+
+function myFunction() {
+    const textpass = document.getElementById("password");
+    if (textpass.type === "password") {
+      textpass.type = "text";
+    } else {
+      textpass.type = "password";
+    }
+  }
